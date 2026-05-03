dafbd3915dfb1963e0b68977fe9a7fd7386f682e
Gold Lourdes Iwe <goldiwe97@gmail.com>
2026-05-03 17:48:16 -0500

    Fix duplicate test method definition

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -141,7 +141,7 @@ def test_get_account_list(self):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-    def test_update_account(self):
+ def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
     

25ecd075a71cf7f983923907495eac99b2f5f65d
Goldie692 <goldiwe97@gmail.com>
2026-05-05 16:42:20 -0400

    Complete rebuild of init file

--- service/__init__.py
+++ service/__init__.py
@@ -1,24 +1,23 @@
- import sys
+import sys
 from flask import Flask
 from flask_talisman import Talisman
 from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 
-# Create Flask application
+# 1. Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
 
-# Initialize Security Headers (Talisman) and CORS
+# 2. Initialize Security Headers (Talisman) and CORS
 talisman = Talisman(app)
 CORS(app)
 
-# Dependencies for the application
+# 3. Import routes and models (to avoid circular imports)
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
-from service import routes, models  # noqa: F401, E402
-from service.common import error_handlers, cli_commands  # noqa: F401, E402
+from service import routes, models  # noqa: F401 E402
+from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
-# Set up logging for production
+# 4. Set up logging
 log_handlers.init_logging(app, "gunicorn.error")
-
 app.logger.info("Service initialized!")

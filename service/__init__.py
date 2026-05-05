87c9bf00700257f359cfb2835f2d4932bf76c3e1
Goldie692 <goldiwe97@gmail.com>
2026-05-05 15:14:00 -0400

    Final syntax fix for service init

--- service/__init__.py
+++ service/__init__.py
@@ -1,7 +1,7 @@
 import sys
 from flask import Flask
-from flask_cors import CORS
 from flask_talisman import Talisman
+from flask_cors import CORS
 from service import config
 from service.common import log_handlers
 

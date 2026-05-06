1ef3ef0cb3c7d147236559d0c24cf39487c8a72c
Goldie692 <goldiwe97@gmail.com>
2026-05-06 14:54:59 -0400

    Fixed indentation and logic in test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -172,19 +172,19 @@ class TestAccountService(TestCase):
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
+        resp = self.client.get(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
-     self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
-
-
-resp = self.client.delete(BASE_URL)
+        resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-    response = self.client.get(
-        "/", environ_overrides=HTTPS_ENVIRON
-    )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
